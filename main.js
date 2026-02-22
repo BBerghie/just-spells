d8aa3da5e27105844d54a14cf58481be8fead27b
Bogdan Berghie <bogdan.berghie@kaytek.es>
2026-02-22 19:07:50 +0100

    clean the ?

--- main.js
+++ main.js
@@ -1,27 +1,81 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  const fragment = document.getElementById("spell-template");
+//const fragment = document.getElementById("spell-template");
 
-  fetch("./resources/spells.json")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+async function loadData() {
+  try {
+    const res = await fetch("./resources/spells.json");
+    const datos = await res.json();
 
-      data.map((item) => {
-        // Create an instance of the template content
-        const instance = document.importNode(fragment.content, true);
+    const spellPool = document.getElementById("spellPool");
 
-        instance.onclick = hgagoalog;
+    datos.forEach((spell) => {
+      const card = document.createElement("li");
+      card.classList.add("card");
 
-        // Add relevant content to the template
-        instance.querySelector(".title").innerHTML = item.englishTitle;
-        // Append the instance ot the DOM
-        document.getElementById("spells").appendChild(instance);
-      });
-    })
-    .catch((error) => console.error("Error loading JSON file", error));
+      card.innerHTML = `
+       	<button>
+      		<div class="front">
+     			<div class="body">
+      				<h3 class="name lined srname">${spell.title} <img src="${getActionImg(spell.actionType)}" alt="${spell.actionType}"/></h3>
+      				<ul class="status lined">
+     					<li><em>Lanzamiento</em>${spell.castTime ? spell.castTime : "-"}</li>
+     					<li class="second"><em>Rango</em>${spell.range}</li>
+     					<br clear="all">
+      				</ul>
 
-  function hgagoalog(params) {
-    console.log("holis");
-    //console.log(params);
+      				<ul class="status lined">
+     					<li><em>Área</em>${spell.area}</li>
+     					<li class="second"><em>Duración</em>${spell.duration}</li>
+     					<br clear="all">
+      				</ul>
+
+      				<ul class="status lined">
+     					<li><em>Objetivo</em>${spell.objectives}</li>
+     					<li class="second"><em>Desencadenate</em>${spell.trigger}</li>
+     					<br clear="all">
+      				</ul>
+      				<p class="text">${spell.description}<br> <b>Elevaciones</b>: ${spell.heightenings} </p>
+
+     			</div>
+     			<b class="class srclass">{spell.traditions}</b>
+     			<b class="type srtype">${spell.type} ${spell.level}</b>
+      		</div>
+      		</button>
+      `;
+
+      // Evento click → llama a una función con el objeto completo
+      card.addEventListener("click", () => selectSpell(spell));
+
+      spellPool.appendChild(card);
+    });
+  } catch (error) {
+    console.error("Error cargando JSON:", error);
   }
-});
+}
+
+function getActionImg(actionType) {
+  let imgPath = "";
+  switch (actionType) {
+    case "one-action":
+      imgPath = "./resources/assets/img/pf2e-action-1.png";
+      break;
+    case "two-actions":
+      imgPath = "./resources/assets/img/pf2e-action-2.png";
+      break;
+    case "three-actions":
+      imgPath = "./resources/assets/img/pf2e-action-3.png";
+      break;
+    case "reaction":
+      imgPath = "./resources/assets/img/pf2e-reaction.png";
+      break;
+    case "free-action":
+      imgPath = "./resources/assets/img/pf2e-free-action.png";
+      break;
+  }
+  return imgPath;
+}
+
+function selectSpell(item) {
+  console.log("Has hecho click en:", item.title);
+}
+
+loadData();
